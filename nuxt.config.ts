793195076e936f2e35dad2b5268ee5c58fb361f9
maku85 <makuma85@gmail.com>
2024-05-08 12:18:39 +0200

    add base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: "/tha-card-club/",
+  },
   modules: ["@nuxt/content", "@nuxtjs/eslint-module", "@nuxtjs/tailwindcss"],
   postcss: {
     plugins: {
